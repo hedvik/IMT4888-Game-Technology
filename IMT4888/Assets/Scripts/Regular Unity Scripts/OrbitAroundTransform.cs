ba11c29f33f5e34a465cfe1a5e6d3840444e4d75
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2018-09-19 14:13:57 +0200

    Added a new Unity scene for galaxy animation This scene is based on the standard Unity workflow so performance is lacking under high "star" counts. A ECS version of this scene will be implemented for performance comparisons.

---
+++ IMT4888/Assets/Scripts/Regular Unity Scripts/OrbitAroundTransform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAroundTransform : MonoBehaviour {
+    public Transform _target;
+    public Vector3 _axis;
+    public float _speed = 5;
+
+    private void Update()
+    {
+        transform.RotateAround(_target.position, _axis, Time.deltaTime * _speed);
+    }
+}
---
+++ IMT4888/Assets/Scripts/Regular Unity Scripts/StarSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSpawner : MonoBehaviour {
+    public int _numberOfStars;
+    public int _numberOfOrbits;
+    public float _orbitOffset;
+    public float _speed;
+    public Vector3 _orbitAxis;
+    public GameObject _starPrefab;
+    public bool _noiseOffset = true;
+    public float _noiseOffsetAmount = 0.5f;
+    public bool _scaleRotationToDistance = true;
+
+    public List<Material> materials = new List<Material>();
+
+    private void Start()
+    {
+        for(int i = 0; i < _numberOfOrbits; i++)
+        {
+            for(int j = 0; j < _numberOfStars/_numberOfOrbits; j++)
+            {
+                var newStar = Instantiate(_starPrefab);
+                var newStarComponent = newStar.GetComponent<OrbitAroundTransform>();
+                newStarComponent._axis = _orbitAxis;
+                newStarComponent._target = transform;
+                newStar.GetComponent<MeshRenderer>().material = materials[Random.Range(0, materials.Count)];
+
+                var randomX = Mathf.Cos(Random.Range(0f, 360f));
+                var randomY = Mathf.Sin(Random.Range(0f, 360f));
+
+                // Might want to make use of the axis here somehow
+                var offsetDirection = new Vector3(randomX, 0, randomY);
+
+                newStar.transform.position = transform.position + (offsetDirection.normalized * (i+1) * _orbitOffset) * (_noiseOffset ? Random.Range(0f, _noiseOffsetAmount) : 1);
+                newStarComponent._speed = _scaleRotationToDistance ? (_speed / (newStar.transform.position - transform.position).magnitude) : _speed;
+            }
+        }
+    }
+}
